946aa352fb69aca8c9c38b3ded3af457665533fa
Mansoor Ahmed <52007922+mans00rahmed@users.noreply.github.com>
2026-07-28 13:10:39 +0100

    Time: 3 ms (84.91%), Space: 21.3 MB (18.93%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,28 +1,15 @@
 class Solution:
-    def majorityElement(self, arr: List[int]) -> int:
-        
-        candidate = -1
-        votes = 0
-            
-        # Finding majority candidate
-        for i in range (len(arr)):
-            if (votes == 0):
-                candidate = arr[i]
-                votes = 1
-            else:
-                if (arr[i] == candidate):
-                    votes += 1
-                else:
-                    votes -= 1
-                    
+    def majorityElement(self, nums: List[int]) -> int:
+        candidate = None
         count = 0
-        for i in range(len(arr)):
-            if arr[i] == candidate:
+        for num in nums:
+            if count == 0:
+                candidate = num
+                count = 1
+            elif num == candidate:
                 count += 1
-
-        if count > len(arr) // 2:
-            return(candidate)
-        else:
-            return("-1")
-             
+            else:
+                count -= 1
         
+        return candidate
+                    
