c37973f94454c0c2b14c0336e7a0ff4abed5ef88
Mansoor Ahmed <52007922+mans00rahmed@users.noreply.github.com>
2026-08-12 13:11:46 +0100

    Time: 0 ms (100%), Space: 19.2 MB (71.79%) - LeetHub

--- 0100-same-tree/0100-same-tree.py
+++ 0100-same-tree/0100-same-tree.py
@@ -7,15 +7,9 @@
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
         # base case(s) go here — comparing p and q, not just checking one
-        if(p is None)and(q is None):
-            return True
-        elif (p is None):
-            return False
-        elif(q is None):
-            return False
-        if (p.val!=q.val):
+        if p is None or q is None:
+            return p is q
+        if p.val != q.val:
             return False
         # recursive calls — but now each call needs a pair too
-        left_same = self.isSameTree(p.left, q.left)
-        right_same = self.isSameTree(p.right, q.right)
-        return True if left_same and right_same else False
+        return True if  self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right) else False
