53cde546584e2b4ad0fc1066735e3280cd69a53b
Mansoor Ahmed <52007922+mans00rahmed@users.noreply.github.com>
2026-09-18 00:25:28 +0100

    Time: 19 ms (58.89%), Space: 35.2 MB (32.89%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -1,7 +1,14 @@
 class Solution:
-    def rotate(self, nums: List[int], k: int) -> None:
+    def rotate(self, nums: list[int], k: int) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
-        k = k % len(nums)
-        nums[:] = nums[-k:] + nums[:-k]
+
+        k = k % len(nums) if len(nums) > 0 else 0
+
+        # Slice from the start to the pivot, and from the pivot to the end
+        first_part = nums[:len(nums)-k]
+        second_part = nums[len(nums)-k:]  # Fixed: changed from nums[k:]
+
+        nums[:] = second_part + first_part
+        
