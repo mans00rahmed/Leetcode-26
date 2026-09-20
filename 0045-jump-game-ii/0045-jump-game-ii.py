23e4dcc59affd2a47a880fd3f9cdd2b820dcf8bd
Mansoor Ahmed <52007922+mans00rahmed@users.noreply.github.com>
2026-09-20 23:46:01 +0100

    Time: 7 ms (51.82%), Space: 20.2 MB (34.57%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -1,33 +1,23 @@
 class Solution:
     def jump(self, nums: list[int]) -> int:
+            if not nums:
+                return 0
 
-        if len(nums) == 1:
-            return 0
+            if all(element == nums[0] for element in nums):
+                return len(nums)-1
 
-        if nums[0] >= len(nums)-1:
-            return 1
+            jump = 0
+            left = 0
+            right = 0
+            while right < len(nums)-1:
+    # ------------------- This is to find max within sub aaray (left to right) ----------------
+                maximum = 0
+                for i in range(left, right+1):
+                    maximum = max(maximum, i + nums[i])
+    # -----------------------------------------------------------------------------------------
+                left = i+1
+                right = maximum
+                jump += 1
 
-        farthestIndex = 0  # 2
-        jump = 0
-        i = 0
-        while (i < len(nums)):
-
-            farthestIndex = max(farthestIndex, i+nums[i])
-            if farthestIndex >= len(nums) - 1:
-                return jump + 1
-
-            if farthestIndex <= i:
-                return -1
-            
-            maxVal = 0
-            maxIndex = -1
-
-            for j in range(i + 1, farthestIndex + 1):
-                if j + nums[j] >= maxVal:
-                    maxVal = j + nums[j]
-                    maxIndex = j
-
-            jump += 1
-            i = maxIndex
-
-        return jump
+            return jump
+    # --------------------------------------------------------------------------
